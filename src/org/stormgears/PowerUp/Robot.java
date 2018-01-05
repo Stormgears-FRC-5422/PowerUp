cfeaf27ab2f803074355d34531fbe2b8e88dbfb8
Michael Pavkovic <mpavkovic688@gmail.com>
2018-01-05 10:59:26 -0500

    Initial commit

---
+++ src/org/stormgears/PowerUp/Robot.java
@@ -0,0 +1,60 @@
+package org.stormgears.PowerUp;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * The entry point of the PowerUp program. Please keep it clean.
+ */
+public class Robot extends IterativeRobot {
+
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+
+	}
+
+	/**
+	 * Runs when autonomous mode starts
+	 */
+	@Override
+	public void autonomousInit() {
+
+	}
+
+	/**
+	 * Runs when teleop mode starts
+	 */
+	@Override
+	public void teleopInit() {
+
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	public void autonomousPeriodic() {
+
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
+
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+
+	}
+}
+
