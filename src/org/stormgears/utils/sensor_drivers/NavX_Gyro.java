7270568b652d0a76b9056f04e568c8a051f4405e
aditya <adityan9900@gmail.com>
2018-01-14 16:37:48 -0500

    added navx gyro class

---
+++ src/org/stormgears/utils/sensor_drivers/NavX_Gyro.java
@@ -0,0 +1,12 @@
+package org.stormgears.utils.sensor_drivers;
+
+public class NavX_Gyro implements Sensor {
+
+    
+
+    public boolean isAvailable() {
+        //TODO: add some sort of if statement
+        return true;
+    }
+
+}
