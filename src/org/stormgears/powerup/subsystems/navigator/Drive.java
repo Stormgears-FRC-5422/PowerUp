d3e5acafe8611e8c8a3851d0db41e7d08cd65e23
Michael Pavkovic <mpavkovic688@gmail.com>
2018-01-14 18:05:36 -0500

    Initial high level move() code. Some stuff needs to be implemented by whoever is writing the drive code.

--- src/org/stormgears/powerup/subsystems/navigator/Drive.java
+++ src/org/stormgears/powerup/subsystems/navigator/Drive.java
@@ -1,26 +1,41 @@
 package org.stormgears.powerup.subsystems.navigator;
 
+import org.stormgears.powerup.Robot;
+import org.stormgears.powerup.subsystems.dsio.JoystickFilter;
+
 public class Drive {
-    private static Drive instance = new Drive();
-    public static Drive getInstance() {
-        return instance;
-    }
+	private static Drive instance = new Drive();
 
-    private Drive() {
+	public static Drive getInstance() {
+		return instance;
+	}
 
-    }
+	// TODO: rename this
+	private static final int RANDOM_CONSTANT_THAT_NEEDS_TO_BE_RENAMED = 6300;
 
-    public void move() {
+	// Configure joystick filters here
+	private static final JoystickFilter[] DRIVE_FILTERS = { JoystickFilter.NULLZONE };
+	private static final JoystickFilter Z_FILTER = JoystickFilter.nullzone(0.2);
 
-//        double theta = Math.atan2(joy.getX(), joy.getY());
-//        if(theta < 0) theta = 2 * Math.PI + theta;
-//
-//        if(Math.abs(joy.getX()) > 0.1 || Math.abs(joy.getY()) > 0.1 || Math.abs(joy.getZ()) > 0.2)
-//            mecMove(6300 * Math.sqrt(joy.getX() * joy.getX() + joy.getY() *
-//                    joy.getY() + joy.getZ() * joy.getZ()), theta, joy.getZ());
-//        else {
-//            setDriveTalonsZeroVelocity();
-//        }
-    }
+	private Drive() {
+
+	}
+
+	public void move() {
+		double x = Robot.dsio.getJoystickX(DRIVE_FILTERS),
+				y = Robot.dsio.getJoystickY(DRIVE_FILTERS),
+				z = Robot.dsio.getJoystickZ(Z_FILTER);
+
+		double theta = Math.atan2(x, y);
+		if (theta < 0) theta = 2 * Math.PI + theta;
+
+		if (x == 0 && y == 0 && z == 0) {
+			// TODO: Implement
+//			setDriveTalonsZeroVelocity();
+		} else {
+			// TODO: Implement
+//			mecMove(RANDOM_CONSTANT_THAT_NEEDS_TO_BE_RENAMED * Math.sqrt(x * x + y * y + z * z), theta, z);
+		}
+	}
 
 }
