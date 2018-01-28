f2a1acd54a0821e79a9cd5780c970bf2dc6c81f7
krisharma <krishsharma1128@gmail.com>
2018-01-28 15:27:44 -0500

    Created class and inner classes for the dimensions of the critical field elements and zones as references and to be used for Global Mapping

--- src/org/stormgears/powerup/subsystems/field/FieldElements.java
+++ src/org/stormgears/powerup/subsystems/field/FieldElements.java
@@ -6,9 +6,56 @@ public class FieldElements {
 	 *
 	 * The _only_ things in here should be inner classes describing major components of the PowerUp field
 	 * Inside of each class, please add only public static final fields describing measurements
+	 *
+	 * Each of the following Inner Classes contains the dimensions of critical field landmarks and zones
 	 */
 
 	public static class Switch {
+		public static final double LENGTH = 0;
+		public static final double WIDTH = 0;
+		public static final double HEIGHT = 0;
+	}
 
+	public static class Scale {
+		public static final double LENGTH = 0;
+		public static final double WIDTH = 0;
+		public static final double HEIGHT = 0;
+	}
+
+	public static class Exchange {
+		public static final double LENGTH = 0;
+		public static final double WIDTH = 0;
+		public static final double HEIGHT = 0;
+
+	}
+
+	public static class Portal {
+		public static final double LENGTH = 0;
+		public static final double WIDTH = 0;
+		public static final double HEIGHT = 0;
+	}
+
+	public static class Platform {
+		public static final double LENGTH = 0;
+		public static final double WIDTH = 0;
+		public static final double HEIGHT = 0;
+	}
+
+	public static class	PlatformZone {
+		public static final double LENGTH = 0;
+		public static final double WIDTH = 0;
+		public static final double HEIGHT = 0;
+	}
+
+	public static class NullZone {
+		public static final double LENGTH = 0;
+		public static final double WIDTH = 0;
+		public static final double HEIGHT = 0;
+	}
+
+	public static class PowerCubeZone {
+		public static final double LENGTH = 0;
+		public static final double WIDTH = 0;
+		public static final double HEIGHT = 0;
 	}
 }
