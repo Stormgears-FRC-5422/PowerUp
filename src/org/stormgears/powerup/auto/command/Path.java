c3c40c22226dd0194ca6c356c6967b02967b77ce
willow2014 <willow.pine.2011@gmail.com>
2018-02-04 15:57:33 -0500

    added Strafe and Follow class for auto.command

--- src/org/stormgears/powerup/auto/command/Path.java
+++ src/org/stormgears/powerup/auto/command/Path.java
@@ -1,4 +1,15 @@
 package org.stormgears.powerup.auto.command;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import com.ctre.CANTalon;
+import com.ctre.phoenix.ErrorCode;
+import com.ctre.phoenix.motion.MotionProfileStatus;
+import com.ctre.phoenix.motion.TrajectoryPoint;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import org.stormgears.powerup.Robot;
+import org.stormgears.utils.RegisteredNotifier;
+import org.stormgears.utils.StormTalon;
 
 public class Path {
 
