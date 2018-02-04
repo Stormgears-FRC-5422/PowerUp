3d86034d8352533df50c5804498579705068462d
krisharma <krishsharma1128@gmail.com>
2018-02-04 13:24:09 -0500

    Global Mapping completed with encoders.

--- src/org/stormgears/powerup/subsystems/navigator/GlobalMapping.java
+++ src/org/stormgears/powerup/subsystems/navigator/GlobalMapping.java
@@ -5,12 +5,14 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.stormgears.powerup.Robot;
 
 public class GlobalMapping {
 
 	static final double PI = Math.PI;
 	static final double WHEEL_RADIUS = 4.0; //inches
 	static final int ENC_RESOLUTION = 2048; //ticks per revolution
+	static final double RADIANS_PER_TICK = 2*Math.PI/(float) ENC_RESOLUTION;
 
 
 	static long enc_fl;
@@ -67,8 +69,55 @@ public class GlobalMapping {
 		//networkPublish(NetworkConstants.GP_VY, vy);
 	}
 
+	public void resetPosition(double X, double Y, double theta) {
+		x = X;
+		y = Y;
+		vel_x = 0;
+		vel_y = 0;
+
+		ahrs.setAngleAdjustment((theta*180/Math.PI)-ahrs.getAngle());
+	}
+
 	public static void updatePos() {
 		//TODO: Get access to talons in order to get the enc position for each talon
-		//enc_fl = Drive.talons
+		enc_fl = Drive.talons[Robot.config.frontLeftTalonId].getSensorCollection().getQuadraturePosition();
+		enc_fr = Drive.talons[Robot.config.frontRightTalonId].getSensorCollection().getQuadraturePosition();
+		enc_bl = Drive.talons[Robot.config.rearLeftTalonId].getSensorCollection().getQuadraturePosition();
+		enc_br = Drive.talons[Robot.config.rearRightTalonId].getSensorCollection().getQuadraturePosition();
+
+		int d_enc_fl = (int) (enc_fl - prev_enc_fl);
+		int d_enc_fr = (int) (enc_fr - prev_enc_fr);
+		int d_enc_bl = (int) (enc_bl - prev_enc_bl);
+		int d_enc_br = (int) (enc_br - prev_enc_br);
+
+		double dRobotX = (d_enc_fr + d_enc_bl - (d_enc_fl + d_enc_br))*RADIANS_PER_TICK*WHEEL_RADIUS/4.f;
+		double dRobotY = (d_enc_fl + d_enc_bl + (d_enc_fl + d_enc_br))*RADIANS_PER_TICK*WHEEL_RADIUS/4.f;
+
+		double angle = getTheta();
+
+		double dFieldX = dRobotX * Math.cos(angle);
+		double dFieldY = dRobotY * Math.cos(angle);
+
+		x += dFieldX;
+		y += dRobotY;
+
+		double dt = Timer.getFPGATimestamp() - prevTimeStamp;
+		double temp_vel_x = dFieldX/dt;
+		double temp_vel_y = dFieldY/dt;
+
+		//TODO: Figure out the smoothing factor code
 	}
+
+	public static double getX() {
+		return x;
+	}
+
+	public static double getY() {
+		return y;
+	}
+
+	public static double getTheta() {
+		return ahrs.getAngle()*Math.PI/180.0;
+	}
+
 }
