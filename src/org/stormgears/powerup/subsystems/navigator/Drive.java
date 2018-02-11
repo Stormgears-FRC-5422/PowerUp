77a7801a3fd19dc7d37ca0fcbd9e66235b2fa6e9
Rohan Prasad <rohanp9000@gmail.com>
2018-02-11 12:49:35 -0500

    Updated Motion Magic profile

--- src/org/stormgears/powerup/subsystems/navigator/Drive.java
+++ src/org/stormgears/powerup/subsystems/navigator/Drive.java
@@ -9,24 +9,34 @@ import org.stormgears.powerup.subsystems.navigator.motionprofile.MotionManager;
 import org.stormgears.powerup.subsystems.navigator.motionprofile.TrapezoidalProfile;
 import org.stormgears.utils.StormTalon;
 
+import java.util.Arrays;
+
 // TODO: CLEAN THIS UP
 public class Drive {
 	private static Drive instance;
-	public static Drive getInstance() { return instance; }
+
+	public static Drive getInstance() {
+		return instance;
+	}
 
 	private static final Logger logger = LogManager.getLogger(Drive.class);
 
 	private static final int MAX_VELOCITY_ENCODER_TICKS = 6300;
-	private static final int TALON_FPID_TIMEOUT = 0;	// TODO: Adithya said 'Figure out what the hell that thing is'
+	private static final int TALON_FPID_TIMEOUT = 0;    // TODO: Adithya said 'Figure out what the hell that thing is'
+	private static int maxVel;
+	private static int maxAccel;
 	private static final ControlMode MODE = ControlMode.Velocity;
 
 	private static MotionMagic[] motions;
 	private MotionManager m = new MotionManager();
+
 	private Drive() {
+
+		maxVel = 15000;
+		maxAccel = 6000;
 		motions = new MotionMagic[Robot.driveTalons.getTalons().length];
-		for(int i = 0; i < Robot.driveTalons.getTalons().length; i ++) {
-			motions[i] = new MotionMagic(Robot.driveTalons.getTalons()[i]);
-		}
+
+
 	}
 
 
@@ -36,14 +46,14 @@ public class Drive {
 
 	public void move() {
 		double x = Robot.dsio.getJoystickX(),
-				y = Robot.dsio.getJoystickY(),
-				z = Robot.dsio.getJoystickZ();
+			y = Robot.dsio.getJoystickY(),
+			z = Robot.dsio.getJoystickZ();
 
 		double theta = Math.atan2(x, y);
 		if (theta < 0) theta = 2 * Math.PI + theta;
 
 		if (x == 0 && y == 0 && z == 0) {
-       		setDriveTalonsZeroVelocity();
+			setDriveTalonsZeroVelocity();
 		} else {
 			mecMove(MAX_VELOCITY_ENCODER_TICKS * Math.sqrt(x * x + y * y + z * z),
 				theta,
@@ -93,13 +103,13 @@ public class Drive {
 		}
 
 		while (Math.abs(vels[0]) > 1.0 ||
-				Math.abs(vels[1]) > 1.0 ||
-				Math.abs(vels[2]) > 1.0 ||
-				Math.abs(vels[3]) > 1.0) {
+			Math.abs(vels[1]) > 1.0 ||
+			Math.abs(vels[2]) > 1.0 ||
+			Math.abs(vels[3]) > 1.0) {
 			double max = Math.max(Math.max(Math.max(Math.abs(vels[0]),
-					Math.abs(vels[1])),
-					Math.abs(vels[2])),
-					Math.abs(vels[3]));
+				Math.abs(vels[1])),
+				Math.abs(vels[2])),
+				Math.abs(vels[3]));
 
 			for (int i = 0; i < vels.length; i++) {
 				vels[i] /= max;
@@ -107,8 +117,8 @@ public class Drive {
 		}
 
 		//Turning in place
-		if(Robot.dsio.getJoystickX() == 0 && Robot.dsio.getJoystickY() == 0) {
-			for(int i = 0; i < vels.length; i ++) {
+		if (Robot.dsio.getJoystickX() == 0 && Robot.dsio.getJoystickY() == 0) {
+			for (int i = 0; i < vels.length; i++) {
 				vels[i] = -changeVel;
 			}
 		}
@@ -137,7 +147,7 @@ public class Drive {
 		double navX_theta = Robot.sensors.getNavX().getTheta();
 		theta = theta + navX_theta;
 
-		double [][] profile = TrapezoidalProfile.getTrapezoidZero(rotations, 300, theta, 0);
+		double[][] profile = TrapezoidalProfile.getTrapezoidZero(rotations, 300, theta, 0);
 		m.pushProfile(profile, false, true);
 		m.startProfile();
 	}
@@ -157,9 +167,11 @@ public class Drive {
 	 * that when calling this method.
 	 *
 	 * @param distance - the distance to move the robot in inches
-	 * @param theta - the angle at which to move the robot
+	 * @param theta    - the angle at which to move the robot
 	 */
-	public void runMotionMagic(double distance, double theta){
+	public void runMotionMagic(double distance, double theta) {
+
+
 		double navX_theta = Robot.sensors.getNavX().getTheta();
 		theta = theta + navX_theta;
 
@@ -167,9 +179,10 @@ public class Drive {
 		double wheelCircumference = 2 * Math.PI * 3; //3 in wheel radius???
 		//TODO: constant for encoder ticks
 		double ticks = distance / wheelCircumference * 8192;
-		motions[0].runMotionMagic((int)ticks);
+		motions[0].runMotionMagic((int) ticks);
 
 		double[] modifiers = new double[motions.length];
+
 		//From the mecMove method...
 		//TODO: test and see if this works
 		modifiers[0] = -(Math.sin(theta + Math.PI / 2.0) + Math.cos(theta + Math.PI / 2.0));
@@ -177,8 +190,32 @@ public class Drive {
 		modifiers[2] = -(Math.sin(theta + Math.PI / 2.0) - Math.cos(theta + Math.PI / 2.0));
 		modifiers[3] = (Math.sin(theta + Math.PI / 2.0) + Math.cos(theta + Math.PI / 2.0));
 
-		for(int i = 0; i < motions.length; i ++) {
-			motions[i].runMotionMagic((int)(ticks * modifiers[i]));
+		int max = 0;
+		for (int i = 0; i < modifiers.length; i++) {
+			if (modifiers[i] > modifiers[max]) {
+				max = i;
+			}
+		}
+
+		double d2 = modifiers[max] * distance;
+		double t1 = maxVel / maxAccel;
+		double totTime = 0; //TODO: FIND TOTAL TIME
+		double vmax2 = d2 / (totTime - t1);
+		double a2 = vmax2 / t1;
+
+		for (int i = 0; i < Robot.driveTalons.getTalons().length; i++) {
+			if (i == 0 || i == 1) {
+				motions[i] = new MotionMagic(Robot.driveTalons.getTalons()[i], vmax2, a2);
+			} else {
+				motions[i] = new MotionMagic(Robot.driveTalons.getTalons()[i], maxVel, maxAccel);
+			}
+		}
+
+		for (int i = 0; i < motions.length; i++) {
+			motions[i].runMotionMagic((int) (ticks * modifiers[i]));
 		}
 	}
+
+
+
 }
