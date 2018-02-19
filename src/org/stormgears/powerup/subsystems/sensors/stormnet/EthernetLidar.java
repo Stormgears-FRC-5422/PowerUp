cd7df320dd9f85e583fdf3d382ff731e178128fb
Vatsal Agarwal <vatsalag99@gmail.com>
2018-02-19 13:45:06 -0500

    Added addresses for the Ethernet LIDAR and cleaned LineIR.

--- src/org/stormgears/powerup/subsystems/sensors/stormnet/EthernetLidar.java
+++ src/org/stormgears/powerup/subsystems/sensors/stormnet/EthernetLidar.java
@@ -4,27 +4,32 @@ import java.util.concurrent.TimeUnit;
 
 public class EthernetLidar extends StormNetSensor {
 	private short[] sensorValues;
+	private byte[] addressValues;
 
 	public EthernetLidar(StormNetVoice voice) {
 		super(voice);
+
 		// TODO magic number
 		setSensorCount(4);
 		sensorValues = new short[m_numSensors];
+		// TODO: 16 addresses are hardcoded on the Mega
+		addressValues = new byte[16];
+
 		this.m_deviceString = voice.getDeviceString();
 	}
 
 	public boolean test(int sleep) {
 		boolean superResult = super.test(sleep);
+		testAddresses(sleep);
 
 		try {
-			short[] sensorValues = new short[4];  // 4 sensor values
 			for (int i = 0; i < sleep; i++) {
-				fetchShorts("L", "Lidar", sensorValues);
+				pollDistance();
 				log("Lidar test returned [ " +
-					sensorValues[0] + " ] [ " +
-					sensorValues[1] + " ] [ " +
-					sensorValues[2] + " ] [ " +
-					sensorValues[3] + " ]");
+				sensorValues[0] + " ] [ " +
+				sensorValues[1] + " ] [ " +
+				sensorValues[2] + " ] [ " +
+				sensorValues[3] + " ]");
 				TimeUnit.SECONDS.sleep(1);
 			}
 		} catch (Exception e) {
@@ -34,14 +39,34 @@ public class EthernetLidar extends StormNetSensor {
 		return true;
 	}
 
+	public void testAddresses(int sleep) {
+		try {
+			for (int i = 0; i < sleep; i++) {
+				pollAddress();
+				log("Lidar Address test returned [ " +
+					addressValues[0] + " ] [ " +
+					addressValues[1] + " ] [ " +
+					addressValues[2] + " ] [ " +
+					addressValues[3] + " ]");
+				TimeUnit.SECONDS.sleep(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public void pollDistance() {
 		fetchShorts("L", "Lidar", sensorValues);
+	}
+
+	public void pollAddress() {
+		fetchBytes("A", "Address", addressValues);
 	}
 
 	// Distance in millimeters
 	public int getDistance(int sensorNumber) {
 		pollDistance();
-		return (0xFFFF & sensorValues[sensorNumber]); // Java wants shorts to be signed.  We want unsigned value
+		return (sensorValues[sensorNumber]); // Java wants shorts to be signed.  We want unsigned value
 	}
 
 
