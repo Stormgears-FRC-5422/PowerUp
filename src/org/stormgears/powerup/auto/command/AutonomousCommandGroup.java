7fac72e68a82b5dc15941f9e4c588202c3e7bcf2
Suren Karavettil <surenck@gmail.com>
2018-02-19 16:18:47 -0500

    Adding initial DRAFT version of Autonomous Move Command

---
+++ src/org/stormgears/powerup/auto/command/AutonomousCommandGroup.java
@@ -0,0 +1,51 @@
+package org.stormgears.powerup.auto.command;
+
+import org.stormgears.powerup.Robot;
+import org.stormgears.powerup.subsystems.field.FieldPositions;
+import org.stormgears.powerup.subsystems.navigator.Position;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class AutonomousCommandGroup extends CommandGroup {
+	private static final Logger logger = LogManager.getLogger(AutonomousCommandGroup.class);
+	public Command autoMoveCommand;
+
+    public AutonomousCommandGroup() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+    
+    public AutonomousCommandGroup(FieldPositions.Alliance selectedAlliance,
+									FieldPositions.StartingSpots selectedStartingSpot,
+									FieldPositions.PlacementSpot selectedPlacementSpot,
+									FieldPositions.LeftRight selectedOwnSwitchPlateAssignment,
+									FieldPositions.LeftRight selectedScalePlateAssignment,
+									FieldPositions.LeftRight selectedOpponentSwitchPlateAssignmentChooser) {
+//		requires(Robot.navigatorSubsystem);
+    	logger.info("creating autonomous command group");
+    	autoMoveCommand = new AutoMoveCommand(selectedAlliance, selectedStartingSpot,
+    										  selectedPlacementSpot, selectedOwnSwitchPlateAssignment,
+    										  selectedScalePlateAssignment, selectedOpponentSwitchPlateAssignmentChooser);
+		addSequential(autoMoveCommand);
+    }    
+}
