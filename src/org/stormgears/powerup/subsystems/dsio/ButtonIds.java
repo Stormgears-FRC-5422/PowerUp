99a108022244f4c0b8f7c0804b9690e31aed6186
Michael Pavkovic <mpavkovic688@gmail.com>
2018-02-20 10:58:01 -0500

    2018 Button IDs and ThreeWaySwitch class template

--- src/org/stormgears/powerup/subsystems/dsio/ButtonIds.java
+++ src/org/stormgears/powerup/subsystems/dsio/ButtonIds.java
@@ -16,24 +16,30 @@ class ButtonIds {
 		}
 
 		public static class Rev2018 {
-			public static final int SCALE_0 = 0;
-			public static final int SCALE_1 = 0;
-			public static final int SCALE_2 = 0;
-			public static final int SCALE_3 = 0;
-			public static final int SCALE_4 = 0;
-			public static final int SWITCH_0 = 0;
-			public static final int SWITCH_1 = 0;
-			public static final int DROP = 0;
-			public static final int SIDE_GO_LEFT = 0;
-			public static final int SIDE_GO_RIGHT = 0;
-			public static final int INTAKE_GRAB = 0;
-			public static final int INTAKE_WHEELS_OUT = 0;
-			public static final int INTAKE_WHEELS_IN = 0;
-			public static final int GRIP_UPPER = 0;
-			public static final int GRIP_LOWER = 0;
-			public static final int CLIMB_UPPER = 0;
-			public static final int CLIMB_LOWER = 0;
-			public static final int OVERRIDE_SWITCH = 0;
+			public static final int SCALE_0 = 8;			// log
+			public static final int SCALE_1 = 5;			// log
+			public static final int SCALE_2 = 7;			// log
+			public static final int SCALE_3 = 10;			// log
+			public static final int SCALE_4 = 6;			// log
+			public static final int SWITCH_0 = 10;  		// msp
+			public static final int SWITCH_1 = 1;			// log
+			public static final int DROP = 14;				// log
+			public static final int SIDE_GO_LEFT = 3;		// log
+			public static final int SIDE_GO_RIGHT = 4;		// log
+			public static final int INTAKE_GRAB = 0;		// log pov left
+			public static final int INTAKE_WHEELS_OUT = 11;	// msp
+			public static final int INTAKE_WHEELS_IN = 5;	// msp
+			public static final int LIFT_OUT = 3;			// msp
+			public static final int LIFT_IN = 12;			// msp
+			public static final int GRIP_UPPER = 14;		// log
+			public static final int GRIP_LOWER = 0;			// log pov up
+			public static final int CLIMB_UPPER = 0;		// log pov right
+			public static final int CLIMB_LOWER = 12;		// log
+			public static final int OVERRIDE_SWITCH = 4;	// msp
+			public static final int JOYSTICK_LEFT = 12;		// msp
+			public static final int JOYSTICK_RIGHT = 13;	// msp
+			public static final int JOYSTICK_UP = 15;		// msp
+			public static final int JOYSTICK_DOWN = 14;		// msp
 		}
 	}
 
