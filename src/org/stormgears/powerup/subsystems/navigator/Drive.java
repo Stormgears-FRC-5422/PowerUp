ffb5b6598fb74cf1f78dd00fc2ed289a8fc779ae
aditya <adityan9900@gmail.com>
2018-02-19 22:58:51 -0500

    Finished turnTo and moveStraight logic/code

--- src/org/stormgears/powerup/subsystems/navigator/Drive.java
+++ src/org/stormgears/powerup/subsystems/navigator/Drive.java
@@ -24,9 +24,8 @@ public class Drive {
 	private static final ControlMode MODE = ControlMode.Velocity;
 
 
-
 	private static final int MAX_VELOCITY = 25000;
-	private static final int MAX_ACCELERATION = 750;
+	private static final int MAX_ACCELERATION = 4800;
 
 	private StormTalon[] talons;
 	private double[] vels;
@@ -70,7 +69,7 @@ public class Drive {
 	}
 
 	public void setVelocityPID() {
-		for(StormTalon t : Robot.driveTalons.getTalons()) {
+		for (StormTalon t : Robot.driveTalons.getTalons()) {
 			t.config_kP(0, Robot.config.velocityP, 10);
 			t.config_kI(0, Robot.config.velocityI, 10);
 			t.config_kD(0, Robot.config.velocityD, 10);
@@ -86,7 +85,7 @@ public class Drive {
 						 double theta,
 						 boolean useAbsoluteControl) {
 
-		for(int i = 0; i < talons.length; i ++) {
+		for (int i = 0; i < talons.length; i++) {
 			talons[i].setInverted(true);
 		}
 
@@ -96,7 +95,7 @@ public class Drive {
 
 		if (useAbsoluteControl) {
 			double navX_theta = Robot.sensors.getNavX().getTheta();
-			theta = theta - navX_theta - (Math.PI/2);
+			theta = theta - navX_theta - (Math.PI / 2);
 		}
 
 		// If +/- 15 degrees of a special angle, assume that angle was the intended direction
@@ -165,7 +164,7 @@ public class Drive {
 			SmartDashboard.putNumber("actualVelocity", actualVelocity);
 			SmartDashboard.putNumber("tractiontest", ((actualVelocity - stickVelocity) / stickVelocity));
 
-				if (stickVelocity > 700 && Math.abs((actualVelocity - stickVelocity) / stickVelocity) > 0.1) {
+			if (stickVelocity > 700 && Math.abs((actualVelocity - stickVelocity) / stickVelocity) > 0.1) {
 				logger.info("Using traction control...");
 
 				double multiplier = 0.5; // (actualVelocity + 0.1) / (vels[0] + 0.1) * 1.1;
@@ -235,23 +234,30 @@ public class Drive {
 	 */
 	public void moveStraight(double distance, double theta) {
 
+//		if (useAbsoluteControl) {
+//			double navX_theta = Robot.sensors.getNavX().getTheta();
+//			theta = theta - navX_theta;
+//		}
 
-		if (useAbsoluteControl) {
-			double navX_theta = Robot.sensors.getNavX().getTheta();
-			theta = theta - navX_theta;
+		if (Math.abs(theta) == Math.PI / 2.0) {
+			distance = distance / 0.833333;
+			System.out.println(distance);
+			System.out.println((distance*8192)/(2 * Robot.config.wheelRadius * Math.PI));
 		}
 
 		//TODO: make wheel diameter and other constants that im just making up
-		double wheelCircumference = 2 * Math.PI * Robot.config.wheelRadius; //4 in wheel radius???
+		double wheelCircumference = 2.0 * Math.PI * Robot.config.wheelRadius;
 		//TODO: constant for encoder ticks
-		double ticks = distance / wheelCircumference * 8192;
+		double ticks = distance / wheelCircumference * 8192.0;
 		// motions[0].runMotionMagic((int) ticks);
+
+
 
 		double[] modifiers = new double[motions.length];
 
 		//From the mecMove method...
 		//TODO: test and see if this works
-		modifiers[0] =  -(Math.sin(theta + Math.PI / 2.0) + Math.cos(theta + Math.PI / 2.0));
+		modifiers[0] = -(Math.sin(theta + Math.PI / 2.0) + Math.cos(theta + Math.PI / 2.0));
 		modifiers[1] = (Math.sin(theta + Math.PI / 2.0) - Math.cos(theta + Math.PI / 2.0));
 		modifiers[2] = -(Math.sin(theta + Math.PI / 2.0) - Math.cos(theta + Math.PI / 2.0));
 		modifiers[3] = (Math.sin(theta + Math.PI / 2.0) + Math.cos(theta + Math.PI / 2.0));
@@ -270,10 +276,12 @@ public class Drive {
 		double a2;
 
 
-		double maxDistance = ((Math.abs(modifiers[max] * distance))* 8192)/(2*Math.PI * Robot.config.wheelRadius);
+		double maxDistance = ((Math.abs(modifiers[max] * distance)) * 8192) / (2 * Math.PI * Robot.config.wheelRadius);
 		for (int i = 0; i < Robot.driveTalons.getTalons().length; i++) {
 
-			currentDistance = ((Math.abs(modifiers[i] * distance))* 8192)/(2*Math.PI * Robot.config.wheelRadius);
+			currentDistance = ((Math.abs(modifiers[i] * distance)) * 8192) / (2 * Math.PI * Robot.config.wheelRadius);
+
+
 			t1 = MAX_VELOCITY / MAX_ACCELERATION;
 			totTime = (t1) + (maxDistance / MAX_VELOCITY) / 10.0; //TODO: FIND TOTAL TIME
 			vmax2 = currentDistance / (totTime - t1) / 10.0;
@@ -281,18 +289,23 @@ public class Drive {
 
 
 			if ((Math.abs(modifiers[i] * distance) != maxDistance)) {
-				System.out.println("Max Distance " + maxDistance);
-				System.out.println("TotalTime " + totTime);
-			motions[i] = new MotionMagic(Robot.driveTalons.getTalons()[i], MAX_VELOCITY, MAX_ACCELERATION);
+				System.out.println("Vmax2 " + vmax2);
+				System.out.println("A2 " + a2);
+				motions[i] = new MotionMagic(Robot.driveTalons.getTalons()[i], vmax2, a2);
+			} else {
+				System.out.println("MAX VEL " + MAX_VELOCITY);
+				System.out.println("MAX ACCEL " + MAX_ACCELERATION);
+				motions[i] = new MotionMagic(Robot.driveTalons.getTalons()[i], MAX_VELOCITY, MAX_ACCELERATION);
+			}
 		}
-	}
 		for (int i = 0; i < motions.length; i++) {
-		System.out.println("Talon " + i + " Commanded: " + (ticks * modifiers[i]));
-		motions[i].runMotionMagic((int) (ticks * modifiers[i]));
+			System.out.println("Talon " + i + " Commanded: " + (ticks * modifiers[i]));
+			motions[i].runMotionMagic((int) (ticks * modifiers[i]));
 		}
 	}
 
-	/** This method takes in an angle, theta, and turns the robot to the
+	/**
+	 * This method takes in an angle, theta, and turns the robot to the
 	 * corresponding direction so that its front faces the angle passed in.
 	 *
 	 * @param theta Angle desired for robot turn
@@ -303,25 +316,50 @@ public class Drive {
 			theta = theta - navX_theta;
 		}
 
+		double negative = 1.0;
+
+
+		theta = theta % (2 * Math.PI);
+		if (theta < 0) {
+			theta += (2 * Math.PI);
+			System.out.println("Theta 2 " + theta);
+		}
+
+		if (theta > Math.PI) {
+			negative = -1.0;
+			System.out.println("Theta 1 " + theta);
+			theta -= Math.PI;
+
+			System.out.println("Rohan blows massive pterodactyl cock" + theta);
+		}
+
 		double robotLength = Double.parseDouble(Robot.config.robotLength);
 		double robotWidth = Double.parseDouble(Robot.config.robotWidth);
 
-		double r = Math.sqrt(Math.pow(robotLength, 2) + Math.pow((robotWidth),2))/2.0;
+		double r = (robotLength + robotWidth) / 3.8;
 
 		double s = r * theta;
 
-		double encoderTicks = s/(2 * Math.PI * Robot.config.wheelRadius) * 8192.0;
+
+		double encoderTicks = s / (2 * Math.PI * Robot.config.wheelRadius) * 8192.0;
 		encoderTicks *= Math.sqrt(2);
 
-		for(int i = 0; i < motions.length; i ++) {
-			motions[i] = new MotionMagic(Robot.driveTalons.getTalons()[i], MAX_VELOCITY /2, MAX_ACCELERATION /2);
+
+		for (int i = 0; i < motions.length; i++) {
+			motions[i] = new MotionMagic(Robot.driveTalons.getTalons()[i], MAX_VELOCITY / 2, MAX_ACCELERATION / 2);
 		}
 
 		System.out.println(encoderTicks + "");
 
 		for (int i = 0; i < motions.length; i++) {
-			System.out.println("Talon " + i + " Commanded: " + (encoderTicks));
-			motions[i].runMotionMagic((int) encoderTicks);
+			if (i == 0 || i == 2) {
+				System.out.println("Talon " + i + " Commanded: " + (encoderTicks));
+				motions[i].runMotionMagic((int) (negative * encoderTicks));
+			} else {
+				System.out.println("Talon " + i + " Commanded: " + (encoderTicks));
+				motions[i].runMotionMagic((int) (negative * encoderTicks));
+
+			}
 		}
 	}
 
@@ -330,23 +368,18 @@ public class Drive {
 	 *
 	 * @param p1 first position
 	 * @param p2 second position
-	 *
 	 */
-	public void moveToPos(Position p1, Position p2){
+	public void moveToPos(Position p1, Position p2) {
 
 		double deltaX = p2.getX() - p1.getX();
 		double deltaY = p2.getY() - p1.getY();
 
-		double theta = Math.atan(deltaY/deltaX);
+		double theta = Math.atan(deltaY / deltaX);
 
 		double hyp = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
 		System.out.println(hyp);
 
 		moveStraight(hyp, theta);
-
-
-
-
 
 
 	}
