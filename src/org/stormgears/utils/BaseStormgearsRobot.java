f6bf2dc2663100b4bd7434ea2850a63cd8c82bbe
Andrew Sun <me@andrewsun.com>
2018-02-20 23:51:39 -0500

    Fix exception handling

--- src/org/stormgears/utils/BaseStormgearsRobot.java
+++ src/org/stormgears/utils/BaseStormgearsRobot.java
@@ -1,8 +1,13 @@
 package org.stormgears.utils;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public abstract class BaseStormgearsRobot extends IterativeRobot {
+	private static final Logger logger = LogManager.getLogger(BaseStormgearsRobot.class);
+
 	private int i = 0;
 
 	public abstract void afterAutonomousInit();
@@ -28,5 +33,17 @@ public abstract class BaseStormgearsRobot extends IterativeRobot {
 	@Override
 	public void disabledInit() {
 		i = 0;
+	}
+
+	@Override
+	public void startCompetition() {
+		// stupid WPILib!
+		try {
+			super.startCompetition();
+		} catch (Throwable e) {
+			logger.fatal("Uncaught exception:");
+			logger.throwing(Level.FATAL, e);
+			throw e;
+		}
 	}
 }
