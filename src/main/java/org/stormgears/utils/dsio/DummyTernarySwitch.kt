df4f746adc24026cabba517c4fc8c4ca9605713c
Andrew Sun <me@andrewsun.com>
2018-02-23 13:23:21 -0500

    Fix missing implementation

--- src/main/java/org/stormgears/utils/dsio/DummyTernarySwitch.kt
+++ src/main/java/org/stormgears/utils/dsio/DummyTernarySwitch.kt
@@ -1,6 +1,10 @@
 package org.stormgears.utils.dsio
 
 class DummyTernarySwitch : DummySwitch(), ITernarySwitch {
+	override fun whenFlippedTernary(listener: (state: ITernarySwitch.SwitchState) -> Unit) {
+
+	}
+
 	override fun whenFlipped(listener: (Boolean) -> Unit) {
 
 	}
