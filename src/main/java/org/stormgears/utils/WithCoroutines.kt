f27499f36ac5c019ab5aa011b0c82c536857deec
Andrew Sun <me@andrewsun.com>
2018-02-26 18:51:50 -0500

    Continuous improvement to coroutines interface

--- src/main/java/org/stormgears/utils/WithCoroutines.kt
+++ src/main/java/org/stormgears/utils/WithCoroutines.kt
@@ -3,16 +3,51 @@ package org.stormgears.utils
 import kotlinx.coroutines.experimental.CoroutineScope
 import kotlinx.coroutines.experimental.CoroutineStart
 import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.channels.ActorScope
+import kotlinx.coroutines.experimental.channels.ProducerScope
 import kotlinx.coroutines.experimental.newSingleThreadContext
 import kotlin.coroutines.experimental.CoroutineContext
-import kotlinx.coroutines.experimental.launch as klaunch
 
 val globalContext = newSingleThreadContext("CoroutineThread");
 
 interface WithCoroutines
 
-fun launch(context: CoroutineContext = globalContext,
-		   start: CoroutineStart = CoroutineStart.DEFAULT,
-		   parent: Job? = null,
-		   block: suspend CoroutineScope.() -> Unit) =
-	kotlinx.coroutines.experimental.launch(context = globalContext, start = start, parent = parent, block = block);
+fun launch(
+	context: CoroutineContext = globalContext,
+	start: CoroutineStart = CoroutineStart.DEFAULT,
+	parent: Job? = null,
+	block: suspend CoroutineScope.() -> Unit
+) = kotlinx.coroutines.experimental.launch(context = context, start = start, parent = parent, block = block);
+
+fun <T> async(
+	context: CoroutineContext = globalContext,
+	start: CoroutineStart = CoroutineStart.DEFAULT,
+	parent: Job? = null,
+	block: suspend CoroutineScope.() -> T
+) = kotlinx.coroutines.experimental.async(context = context, start = start, parent = parent, block = block);
+
+fun <E> produce(
+	context: CoroutineContext = globalContext,
+	capacity: Int = 0,
+	parent: Job? = null,
+	block: suspend ProducerScope<E>.() -> Unit
+) = kotlinx.coroutines.experimental.channels.produce(
+	context = context,
+	capacity = capacity,
+	parent = parent,
+	block = block
+)
+
+fun <E> actor(
+	context: CoroutineContext = globalContext,
+	capacity: Int = 0,
+	start: CoroutineStart = CoroutineStart.DEFAULT,
+	parent: Job? = null,
+	block: suspend ActorScope<E>.() -> Unit
+) = kotlinx.coroutines.experimental.channels.actor(
+	context = context,
+	capacity = capacity,
+	start = start,
+	parent = parent,
+	block = block
+)
