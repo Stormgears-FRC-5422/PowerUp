6defd0fac4cdaf229a97a7d96927956396fe0dd0
Michael Pavkovic <mpavkovic688@gmail.com>
2018-03-09 21:45:16 -0500

    Realbot talon IDs

--- src/main/java/org/stormgears/powerup/TalonIds.java
+++ src/main/java/org/stormgears/powerup/TalonIds.java
@@ -4,11 +4,11 @@ public class TalonIds {
 	/*
 	 * Put non-drive talon IDs here
 	 */
-	public static final int INTAKE_LEFT = 23;
-	public static final int INTAKE_RIGHT = 3;
-	public static final int INTAKE_ART = 20;
+	public static final int INTAKE_LEFT = 10;
+	public static final int INTAKE_RIGHT = 33;
+	public static final int INTAKE_ART = 3;
 	public static final int ELEVATOR_MASTER = 11;
-	public static final int ELEVATOR_SLAVE = 0;
-	public static final int SIDESHIFT = 5;
-	public static final int GRIPPER = 21;
+	public static final int ELEVATOR_SLAVE = 10;
+	public static final int SIDESHIFT = 40;
+	public static final int GRIPPER = 30;
 }
