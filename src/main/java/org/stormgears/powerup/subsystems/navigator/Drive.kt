f5d922e6f81cd49fa5ff4abf446726aaaaa33c1b
Andrew Sun <me@andrewsun.com>
2018-03-28 17:57:39 -0400

    implement possibly better motion profile

--- src/main/java/org/stormgears/powerup/subsystems/navigator/Drive.kt
+++ src/main/java/org/stormgears/powerup/subsystems/navigator/Drive.kt
@@ -326,8 +326,6 @@ object Drive : TerminableSubsystem() {
 		val initPosFL = -talonFL.sensorCollection.quadraturePosition
 		val initPosFR = talonFR.sensorCollection.quadraturePosition
 
-		val maxVel = if (dist < 150) 2000 else 5000
-
 		var avgPos: Double
 		var progress: Double
 		do {
@@ -335,7 +333,8 @@ object Drive : TerminableSubsystem() {
 
 			progress = sign * avgPos / distanceTicks
 
-			val currVel = Math.sin((progress * 0.82 + 0.1) * Math.PI) * maxVel // ticks/100ms
+//			val currVel = Math.sin((progress * 0.82 + 0.1) * Math.PI) * maxVel // ticks/100ms
+			val currVel = sunProfile(progress * dist, dist)
 
 			val currTheta = -sensors.navX.getTheta(NavX.AngleUnit.Degrees, false) // degrees TODO why is this inverted
 			val delta = currTheta - initTheta // degrees
@@ -391,7 +390,7 @@ object Drive : TerminableSubsystem() {
 		logger.trace("initPos = {} targets = {}", Arrays.toString(initPos), Arrays.toString(targets))
 
 //		val maxVel = if (dist < 150) 2000 else 4000
-		val maxVel = 4000
+//		val maxVel = 4000
 
 		var avgPos: Double
 		var progress: Double
@@ -400,7 +399,8 @@ object Drive : TerminableSubsystem() {
 			targets.forEachIndexed { i, target -> progress += abs(((abs(talons[i].sensorCollection.quadraturePosition - initPos[i]))) / abs(target)) }
 			progress /= 4
 
-			val currVel = Math.sin((progress * 0.82 + 0.1) * Math.PI) * maxVel // ticks/100ms
+//			val currVel = Math.sin((progress * 0.82 + 0.1) * Math.PI) * maxVel // ticks/100ms
+			val currVel = sunProfile(progress * dist, dist)
 
 			val currTheta = -sensors.navX.getTheta(NavX.AngleUnit.Degrees, false) // degrees TODO why is this inverted
 			val delta = currTheta - initTheta // degrees
---
+++ src/main/java/org/stormgears/powerup/subsystems/navigator/SunProfile.kt
@@ -0,0 +1,73 @@
+package org.stormgears.powerup.subsystems.navigator
+
+import kotlin.math.PI
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.sin
+
+// See: https://www.desmos.com/calculator/urbpj5x9fg
+
+/**
+ * Target maximum velocity in ticks/100ms
+ */
+val vTarget = 5000.0
+
+/**
+ * Maximum initial velocity in ticks per 100ms
+ */
+val vI = 500.0
+
+/**
+ * Absolute minimum velocity in ticks per 100ms
+ */
+val vMin = 100.0
+
+/**
+ * Maximum acceleration in ticks per 100ms per inch given distance [d] in inches
+ */
+fun maxA(d: Double): Double { // ticks per 100ms per inch???
+	return min(300.0, d * 6.0 + 50.0)
+}
+
+/**
+ * Sine curve correction factor for [rBase]
+ */
+fun vI(y: Double): Double {
+	return min(y * 0.2, vI)
+}
+
+/**
+ * Function of the ramp up and ramp down curves
+ */
+fun rBase(x: Double, xT: Double, yT: Double): Double {
+	val vI1 = vI(yT)
+	return max((sin(PI * (x / xT - 0.5)) + 1) * ((yT - vI1) / 2) + vI1, vMin)
+}
+
+/**
+ * Calculates rBase target x given [y] and distance [d]
+ */
+fun mX(y: Double, d: Double): Double {
+	return min(PI * y / (2 * maxA(d)), d / 2)
+}
+
+/**
+ * Calculates rBase target y given [x] and distance [d]
+ */
+fun mY(x: Double, d: Double): Double {
+	return 2 * maxA(d) * x / PI
+}
+
+/**
+ * Calculates the PID velocity of a motor given movement progress [x] in inches and target distance [d] in inches
+ */
+fun sunProfile(x: Double, d: Double): Double {
+	val mX = mX(vTarget, d)
+	val mYmX = mY(mX, d)
+	return when {
+		x < mX -> rBase(x, mX, mYmX)
+		x in mX..(d - mX) -> vTarget
+		x in (d - mX)..d -> rBase(x - d, mX, mYmX)
+		else -> 0.0
+	}
+}
