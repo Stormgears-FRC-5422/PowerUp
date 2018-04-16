66dc36cc5dae9a68e60476aa255d0ddb241ca2ef
Michael Pavkovic <mpavkovic688@gmail.com>
2018-04-16 11:26:25 -0400

    Added 2018 version 2 button board configuration, along with JoystickAxisButton

---
+++ src/main/java/org/stormgears/powerup/subsystems/dsio/ButtonBoard2018V2.kt
@@ -0,0 +1,65 @@
+package org.stormgears.powerup.subsystems.dsio
+
+import edu.wpi.first.wpilibj.Joystick
+import org.stormgears.utils.dsio.*
+
+class ButtonBoard2018V2 private constructor(jumperGamepad: Joystick, normalGamepad: Joystick, realJoystick: LogitechJoystick?) : IButtonBoard {
+
+	override val gripOpenButton: IButton = EnhancedButton(normalGamepad, 8)
+	override val gripCloseButton: IButton = EnhancedButton(normalGamepad, 9)
+
+	override val dropButton: IButton = EnhancedButton(normalGamepad, 1)
+
+	override val switch0Button: IButton = EnhancedButton(jumperGamepad, 5)
+	override val switch1Button: IButton = EnhancedButton(jumperGamepad, 9)
+
+	override val sideLeftButton: IButton = EnhancedButton(jumperGamepad, 6)
+	override val sideRightButton: IButton = EnhancedButton(jumperGamepad, 10)
+
+	override val intakeGrabButton: IButton = EnhancedButton(normalGamepad, 5)
+
+	override val climbUpButton: IButton = EnhancedButton(normalGamepad, 11)
+	override val climbDownButton: IButton = EnhancedButton(normalGamepad, 10)
+
+	override val scaleButtons: Array<IButton> = arrayOf(
+		EnhancedButton(jumperGamepad, 7),
+		EnhancedButton(jumperGamepad, 8),
+		EnhancedButton(normalGamepad, 2),
+		EnhancedButton(normalGamepad, 3),
+		EnhancedButton(normalGamepad, 4)
+	)
+
+	override val intakeWheelsSwitch: ISwitch = TernarySwitch(jumperGamepad, 11, 12)
+	override val intakeLiftSwitch: ISwitch = TernarySwitch(normalGamepad, 7, 6)
+
+	override val forceZeroElevatorButton: IButton = if (realJoystick != null) EnhancedButton(realJoystick, 7) else DummyButton()
+	override val zeroElevatorButton: IButton = if (realJoystick != null) EnhancedButton(realJoystick, 8) else DummyButton()
+
+	override val overrideSwitch: ISwitch = SwitchControl(normalGamepad, 12)
+	override val overrideUp: IButton = JoystickAxisButton(jumperGamepad, 0, JoystickAxisButton.Direction.Left)
+	override val overrideDown: IButton = JoystickAxisButton(jumperGamepad, 0, JoystickAxisButton.Direction.Right)
+	override val overrideLeft: IButton = JoystickAxisButton(jumperGamepad, 1, JoystickAxisButton.Direction.Right)
+	override val overrideRight: IButton = JoystickAxisButton(jumperGamepad, 1, JoystickAxisButton.Direction.Left)
+
+	override val speedPot: IJoystickAxis = DummyJoystickAxis()
+
+	companion object {
+		private var instance: ButtonBoard2018V2? = null
+
+		@JvmStatic // TODO: Replace with more idiomatic Kotlin (object???)
+		fun getInstance(jumperGamepad: Joystick, normalGamepad: Joystick, realJoystick: LogitechJoystick?): IButtonBoard {
+			if (instance == null) {
+				if (IButtonBoard.initialized) {
+					throw IllegalStateException("Only one button board can exist at once.")
+				}
+
+				IButtonBoard.initialized = true
+
+				instance = ButtonBoard2018V2(jumperGamepad, normalGamepad, realJoystick)
+				return instance as IButtonBoard
+			}
+
+			return instance as IButtonBoard
+		}
+	}
+}
---
+++ src/main/java/org/stormgears/utils/dsio/JoystickAxisButton.kt
@@ -0,0 +1,55 @@
+package org.stormgears.utils.dsio
+
+import edu.wpi.first.wpilibj.Joystick
+import edu.wpi.first.wpilibj.buttons.Button
+import org.apache.logging.log4j.LogManager
+import org.stormgears.utils.LambdaCommand
+import org.stormgears.utils.RunnableCommand
+
+class JoystickAxisButton(private val joystick: Joystick, private val axis: Int, private val direction: Direction) : Button(), IButton {
+	enum class Direction(private val start: Double, private val end: Double) {
+		Right(0.9, 1.0),
+		Left(-1.0, -0.9);
+
+		internal fun match(d: Double): Boolean {
+			return d in start..end
+		}
+	}
+
+	init {
+		this.whenPressed { logger.trace("Joystick axis button {} pressed - {}", direction, joystick.name) }
+	}
+
+	override fun get(): Boolean {
+		val d = this.joystick.getRawAxis(axis)
+		return direction.match(d)
+	}
+
+	override fun whenPressed(callback: Runnable) {
+		super.whenPressed(RunnableCommand(callback))
+	}
+
+	override fun whenPressed(callback: () -> Unit) {
+		super.whenPressed(LambdaCommand(callback))
+	}
+
+	override fun whenReleased(callback: Runnable) {
+		super.whenReleased(RunnableCommand(callback))
+	}
+
+	override fun whenReleased(callback: () -> Unit) {
+		super.whenReleased(LambdaCommand(callback))
+	}
+
+	override fun whileHeld(callback: Runnable) {
+		super.whenReleased(RunnableCommand(callback))
+	}
+
+	override fun whileHeld(callback: () -> Unit) {
+		super.whenReleased(LambdaCommand(callback))
+	}
+
+	companion object {
+		private val logger = LogManager.getLogger(POVButton::class.java)
+	}
+}
